031edc690d46804adc7492575dbb4f57c1649c3c
Arthur Fox <arthur.fox@hotmail.co.uk>
2020-02-20 01:14:14 -0300

    Updates that Simone made
    Added a lot of links to other Limud websites, otherwise mainly made small changes here and there

--- v1.0/sendemail.php
+++ v1.0/sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "Simone Licht" );
-define( "RECIPIENT_EMAIL", "simone.licht@globo.com" );
+define( "RECIPIENT_NAME", "John Doe" );
+define( "RECIPIENT_EMAIL", "youremail@mail.com" );
 
 
 // Read the form values
